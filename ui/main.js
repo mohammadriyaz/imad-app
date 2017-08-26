7e468ba26ee074aa242e9194a29c526876ce983f
riyazuddin ansari <riyazstone23@gmail.com>
2017-08-27 01:40:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,11 +7,10 @@ element.innerHTML = 'New value';
 //Move the image
 var img = document.getElementById('madi');
 var marginLeft = 0;
-function marginRight () {
+function moveRight () {
     marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function() {
     var interval = setInterval(moveRight, 100);
-    
 };
