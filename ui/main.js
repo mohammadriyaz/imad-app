e6b8dd253cf3d8cd7352498ba0325dd1b3096813
riyazuddin ansari <riyazstone23@gmail.com>
2018-02-20 15:00:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,10 +4,21 @@ var counter = 0;
 
 button.onclick = function() {
     
-    // Make a request to the counter endpoint
+    // Create request object
+    var request = new XMLHttpRequest();
+    
     //Capture the response and share it in a variable
-    //Render the varaible in the correct span
-    counter +=1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            //Take some action
+            if(request.status === 200) {
+                var counter = request.responseText;
+                var span = documet.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        // Not done yet
+    };
+    request.open('GET','http://riyazstone23.imad.hasura-app.io', true);
+    request.send(null);
 };
