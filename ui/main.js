b34ecdf71acf40cd82d5be8db94c0313be4f5252
riyazuddin ansari <riyazstone23@gmail.com>
2018-02-20 17:04:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,6 +34,6 @@ submit.onclick = function() {
     for (var i= 0; i<names.length; i++) {
         list += '<li>' + names[i] + '</li>';
     }
-    var ul =document.getElementById('namelist);
+    var ul =document.getElementById('namelist');
     ul.innerHTML = list;
 };
