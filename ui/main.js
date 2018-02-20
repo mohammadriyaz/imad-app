90457e5a36a427a21f76a26ceb824ea8b2f7ff05
riyazuddin ansari <riyazstone23@gmail.com>
2018-02-20 17:45:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,8 +23,6 @@ button.onclick = function() {
 };
 
 //Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     
@@ -49,6 +47,9 @@ submit.onclick = function() {
         }
         // Not done yet
     };
+    //Make the request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET','http://riyazstone23.imad.hasura-app.io/submit-name?name='+name, true);
     request.send(null);
     
